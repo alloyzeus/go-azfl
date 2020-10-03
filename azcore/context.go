e3a6481a85667b55ddc56cc41d959c12a98eb151
Fahrezal Effendi <exavolt@gmail.com>
2020-10-03 08:47:06 +0000

    Add HumanContext

--- azcore/context.go
+++ azcore/context.go
@@ -1,4 +1,20 @@
 package azcore
 
+import (
+	"golang.org/x/text/language"
+)
+
 // Context is a generalized context for all service methods.
 type Context interface{}
+
+// HumanContext is a specialized context, where current processing
+// was initiated by a human.
+type HumanContext interface {
+	Context
+
+	// AcceptLanguage is analogous to HTTP Accept-Language header. The
+	// languages must be ordered by the human's preference.
+	// If the languages comes as weighted, as found in HTTP Accept-Language,
+	// sort the languages by their weights then drop the weights.
+	AcceptLanguage() []language.Tag
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/alloyzeus/go-azcore
 
 go 1.15
+
+require golang.org/x/text v0.3.3
