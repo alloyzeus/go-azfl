68d9781e898483e3a2a243101718e0ce18d43487
Fahrezal Effendi <exavolt@gmail.com>
2021-02-18 07:32:31 +0000

    Update AZWireObject interface

--- azcore/azwire.go
+++ azcore/azwire.go
@@ -5,9 +5,22 @@ package azcore
 // AZWireObject provides an interface for objects which could be encoded
 // to / decoded from azwire encoding.
 type AZWireObject interface {
+	AZWireMarshalable
+
+	// Commented-out as we need to pass around pointers.
+	// AZWireUnmarshalable
+}
+
+// AZWireMarshalable is an interface for objects which could be marshaled
+// with azwire encoding.
+type AZWireMarshalable interface {
 	// AZWire returns azwire-encoded bytes representing the instance.
 	AZWire() []byte
+}
 
+// An AZWireUnmarshalable is an object which could load azwire-encoded
+// bytes into itself.
+type AZWireUnmarshalable interface {
 	// UnmarshalAZWire modifies the instance with data from
 	// azwire-encoded bytes.
 	UnmarshalAZWire(b []byte) error
