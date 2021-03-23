faeb9b3c0446476738c2ba05fdfa4a52b3a5cdf6
Fahrezal Effendi <exavolt@gmail.com>
2021-03-23 10:18:15 +0000

    Change interface UserIDNum and UserRefKey

--- azfl/user.go
+++ azfl/user.go
@@ -1,15 +1,16 @@
 package azcore
 
 // UserID abstracts the identifiers of User entity instances.
-type UserID interface {
+type UserIDNum interface {
 	IDNum
-	AZUserID()
+
+	AZUserIDNum()
 }
 
 // UserRefKey is used to refer to a User entity instance.
 type UserRefKey interface {
 	RefKey
 
-	// UserID returns only the ID part of this ref-key.
-	UserID() UserID
+	// UserIDNum returns only the ID part of this ref-key.
+	UserIDNum() UserIDNum
 }
