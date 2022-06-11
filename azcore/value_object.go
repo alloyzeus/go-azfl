60596f233d22efab180cf7cc9b34a7f9417fa8e3
Fahrezal Effendi <exavolt@gmail.com>
2022-06-11 13:43:20 +0000

    azfl: Add ValueObjectAssert

--- azcore/value_object.go
+++ azcore/value_object.go
@@ -1,5 +1,7 @@
 package azcore
 
+import "github.com/alloyzeus/go-azfl/azob"
+
 // A ValueObject is a small object that represents a simple entity whose
 // equality is not based on identity, i.e., two value objects are equal when
 // they have the same value, not necessarily being the same object.
@@ -7,4 +9,15 @@ type ValueObject interface {
 	//TODO: we should have Clone method contract here, but it requires
 	// covariant support which won't happen
 	// https://github.com/golang/go/issues/30602
+}
+
+// ValueObjectAssert is tool to assert that a struct conforms the
+// characteristic of a value-object.
+//
+// To use:
+//
+//     var _ = ValueObjectAssert[MyStruct] = MyStruct{}
+//
+type ValueObjectAssert[T any] interface {
+	azob.CloneableAssert[T]
 }
